cee8aee4c54a3547edd09022849ad27cd3a91fe6
Miacarmen <maitreyacarmen@gmail.com>
2022-08-30 15:15:45 -0700

    function added to display all searched images

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ form.addEventListener('submit', async function (event) {
   event.preventDefault();
   console.log('Submitted!');
   const searchTerm = form.elements.query.value;
-  
-  const queryUrl = `https://api.tvmaze.com/search/shows?q=${searchTerm}`;
+  // config gets added to end of query string
+  const config = { params: { q: searchTerm } };
 
-  const res = await axios.get(queryUrl);
-  console.log(res.data);
+  const res = await axios.get('https://api.tvmaze.com/search/shows', config);
+  postImages(res.data);
+  // reset search form
+  form.elements.query.value = '';
 });
+
+const postImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement('IMG');
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
